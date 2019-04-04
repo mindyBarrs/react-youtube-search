1e26725dfcefff5b509d654b99f57e0f488ae4e8
Min <barrs.mindy@gmail.com>
2019-04-04 16:12:28 -0400

    Displayed the selected video title and description

--- src/component/VideoDetail/VideoDetail.js
+++ src/component/VideoDetail/VideoDetail.js
@@ -7,7 +7,15 @@ const VideoDetail = ({ video }) => {
     
     return(
         <div>
-            { video.snippet.title }
+            <div className="ui segment" >
+                <h4 className="ui header" >
+                { video.snippet.title }
+                </h4>
+
+                <p>
+                    { video.snippet.description }
+                </p>
+            </div>
         </div>
     )
 }
